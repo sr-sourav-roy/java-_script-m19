88e9a3b7685863896572ac985b28b8603b100444
sr-sourav-roy <sourav.cp.bd@gmail.com>
2026-01-16 23:51:05 +0600

    git for two prolem slove

--- even.js
+++ even.js
@@ -1,14 +1,42 @@
-// even number output
+// ❤️even and odd number output
 
-let num = 1;
+let num = 61;
+// while (num <= 100) {
+//     // console.log(num)
+//     if (num % 2 === 0) {   //even number concept
+//         console.log("This is even nuber..", num)
+//     }
+//     if (num % 2 === 1) {
+//         console.log("This is odd number..", num)
+//     }
+//     num += 1;
+// }
 
-while (num <= 10) {
-    // console.log(num)
-    if (num % 2 === 0) {   //even number concept
-        console.log("This is even nuber..", num)
-    }
-    if (num % 2 === 1) {
-        console.log("This is odd number..", num)
-    }
-    num += 1;
-}
+// ❤️Subtask-1:
+// Find all the odd numbers from 61 to 100.
+let odd = 61;
+// while (odd <= 100) {
+//   if (odd % 2 === 1) {
+//     console.log("It was a odd number:", odd);
+//   }
+//   odd +=1
+// }
+
+
+// ❤️Subtask-2:
+// Find all the even numbers from 78 to 98
+let even = 78;
+// while (even <= 98) {
+//     if (even % 2 === 0) {
+//         console.log("It was a even..", even)
+//     }
+//     even += 1
+// }
+
+
+// ❤️this message 60 times. So display
+let count = 1;
+// while (count <= 60) {
+//   console.log("This message", count);
+//   count++;
+// }
--- for-sum.js
+++ for-sum.js
@@ -1,4 +1,4 @@
-// for loop short_cut sentext
+// ❤️for loop short_cut sentext
 let sum = 0; 
 // for (let i = 10; i <= 30; i += 1){
 //     sum = sum + i;
@@ -6,11 +6,48 @@ let sum = 0;
 // }
 // console.log(sum)
 
-// while loop short _cut sentext
+// ❤️while loop short _cut sentext
 let num = 1;
 let sums = 0;
 // while (num <= 20) {
 //     sums = sums + num;
 //     num += 1;
 // }
-// console.log(sums)  
+// console.log(sums)
+
+// ❤️Subtask-1:
+// Find all the odd numbers from 61 to 100.
+let odd = [];
+// for (let i = 61; i <= 100; i += 1){
+//     if (i % 2 === 1) {
+//         odd.push(i);
+//     }
+// }
+// console.log("odd number..:", odd);
+
+
+// ❤️Subtask-2:
+// Find all the even numbers from 78 to 98
+let even = [];
+// for (let i = 78; i <= 98; i += 1){
+//     if (i % 2 == 0) {
+//         even.push(i);
+//     }
+// }
+// console.log("It was even_Number:",even)
+
+
+// ❤️this message 60 times. So display
+// for (let i = 1; i <= 60; i += 1){
+//     console.log("This message", i)
+// }
+
+
+// ❤️Subtask-1:
+// Display sum of all the odd numbers from 81 to 131.
+let total = 0;
+for (let i = 81; i <= 131; i += 1){
+    total = total + i;
+    // console.log(i)
+}
+console.log("total score:",total)
